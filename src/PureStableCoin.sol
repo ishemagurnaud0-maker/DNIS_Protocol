900182fc458befc2798274f29f829d44ddbfa8d5
Gurnaud <ishemagurnaud0@gmail.com>
2026-09-23 18:27:24 +0000

    [update]: The PureStableCoin updated contract

--- src/PureStableCoin.sol
+++ src/PureStableCoin.sol
@@ -2,8 +2,60 @@
 pragma solidity ^0.8.30;
 
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PureStableCoin {
-    
+/*
+* @title Pure StableCoin Contract
+* @author ISHEMA Gurnaud
+* Collateral: Exogenous(ETH & BTC)
+* Minting Stability: Algorithmic
+* Relative Stability: Anchored to USD (dollar)
+
+*
+* This is the contract met to be governed by the PREngine. This is an ERC20 implementation
+  for the ERC20 token.
+
+ */
+
+contract PureStableCoin is ERC20Burnable, Ownable {
+
+    error PureStableCoin__FundsMustBeGreaterFunds();
+    error PureStableCoin__InsufficientFunds();
+    error PureStableCoin__NotZeroAddress();
+
+
+    constructor() ERC20("PURE", "PR") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner{
+    uint256 balance = balanceOf(msg.sender);
+
+    if(_amount <= 0) {
+        revert PureStableCoin__FundsMustBeGreaterFunds();
+    }
+
+    if(balance < _amount) {
+        revert PureStableCoin__InsufficientFunds();
+    }
+
+    super.burn(_amount);
+}
+
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns(bool) {
+        if(_to == address(0)) {
+            revert PureStableCoin__NotZeroAddress();
+        }
+
+            if(_amount <= 0) {
+                revert PureStableCoin__FundsMustBeGreaterFunds();
+            }
+
+            _mint(_to, _amount);
+
+           return true; 
+    }
+
+
+
 }   
