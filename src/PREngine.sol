cbd29bf80d9ddcf8848267b8ac0818010d304e21
Gurnaud <ishemagurnaud0@gmail.com>
2026-09-23 21:06:18 +0000

    [refactor]: The PREngine implemented the deposit collateral funciton.

--- src/PREngine.sol
+++ src/PREngine.sol
@@ -16,5 +16,64 @@ pragma solidity ^0.8.30;
 
  * It is similar to DAI if DAI had no governance , no fees and was only backed by wETH and wBTC.
  *
- * @notice This contract is the core of the PURE StableCoin
+ * @notice This contract is the core of the PURE StableCoin System. 
  */
+
+
+ contract PREngine {
+
+        error PREngine__MustBeGreaterThanZero();
+        error PREngine__InvalidAddress();
+        error PREngine__TokenAddressesExceedPriceFeeds();
+
+
+      mapping(address token => address priceFeed) private s_tokenToPriceFeed;
+        
+
+         modifier NotZero(uint256 _amount) {
+            if(_amount <= 0) {
+                revert PREngine__MustBeGreaterThanZero();
+            }
+
+            _;
+         }
+
+
+         constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address PRCoinAddress) {
+            if(tokenAddresses.length != priceFeedAddresses.length) {
+               revert PREngine__TokenAddressesExceedPriceFeeds();
+            }
+
+            for(uint256 i = 0; i < tokenAddresses.length; i++){
+               s_tokenToPriceFeed[tokenAddresses[i]] = priceFeedAddresses[i];
+            }
+         }
+
+    function depositCollateralAndMintPRCoin() external {}
+
+
+    /*
+    * @param tokenCollateralAddress The address of the token to deposit as collateral
+    * @param amountCollateral The amount of collateral that you are to put up in order to borrow the PURE stablecoin
+     
+    */
+
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral) external NotZero(amountCollateral){
+         if(tokenCollateralAddress == address(0)) {
+            revert PREngine__InvalidAddress();
+         }
+
+    }
+
+    function redeemCollateralForPRCoin() external {}
+
+    function redeemCollateral() external {}
+
+    function liquidate() external {}
+
+    function burnPRCoin() external {}
+
+    function mintPRCoin() external {}
+
+    function getHealthFactor() external view {}
+ }
